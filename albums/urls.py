6dfb57d53f02a5c86a5d063257884081f8f8a6e2
dias0092 <muihdias25@gmail.com>
2023-07-09 16:23:50 +0600

    Added CRUD views , templates and URLs for the albums app

---
+++ albums/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import album_list, album_detail, album_new, album_edit, album_delete
+
+urlpatterns = [
+    path('albums/', album_list, name='album_list'),
+    path('album/<int:pk>/', album_detail, name='album_detail'),
+    path('album/new/', album_new, name='album_new'),
+    path('album/<int:pk>/edit/', album_edit, name='album_edit'),
+    path('album/<int:pk>/delete/', album_delete, name='album_delete'),
+    ]
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Album
+from .forms import AlbumForm
 
-# Create your views here.
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'albums/album_list.html', {'albums': albums})
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'albums/album_detail.html', {'album': album})
+
+def album_new(request):
+    if request.method == "POST":
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            album = form.save(commit=False)
+            album.save()
+            return redirect('album_detail', pk=album.pk)
+    else:
+        form = AlbumForm()
+    return render(request, 'albums/album_edit.html', {'form': form})
+
+def album_edit(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == "POST":
+        form = AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            album = form.save(commit=False)
+            album.save()
+            return redirect('album_detail', pk=album.pk)
+    else:
+        form = AlbumForm(instance=album)
+    return render(request, 'albums/album_edit.html', {'form': form})
+
+def album_delete(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == "POST":
+        album.delete()
+        return redirect('album_list')
+    return render(request, 'albums/album_confirm_delete.html', {'album': album})
