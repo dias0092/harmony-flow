127fa6d7ba64bea7b085624f90d011bc54f1ec08
dias0092 <muihdias25@gmail.com>
2023-07-09 13:49:48 +0600

    Added the Album model in the albums app

---
+++ albums/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from artists.models import Artist
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    image = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
