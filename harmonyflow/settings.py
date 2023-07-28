465133794318bdb25efa019a87b662932842f468
Ubuntu <dias@hflow.hov51vllsmou3nifgigkukkfvg.bx.internal.cloudapp.net>
2023-07-28 08:23:04 +0000

    Add requirements.txt for dependency management

--- harmonyflow/settings.py
+++ harmonyflow/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = 'harmonyflow.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'harmonyflow',
+        'USER': 'dias',
+        'PASSWORD': '0706',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
