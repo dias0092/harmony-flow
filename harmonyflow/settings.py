9b0dd8c3728b9c4dde965e0e5e8a58c2ae055ee7
Dias Mukhametarkhim <54438346+dias0092@users.noreply.github.com>
2023-07-28 08:08:58 +0000

    Removed old migrations from SQLlite

--- harmonyflow/settings.py
+++ harmonyflow/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'harmonyflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'harmonyflow',
+        'USER': 'dias',
+        'PASSWORD': '0706',
+        'HOST': '52.188.62.42',
+        'PORT': '',
     }
 }
 
