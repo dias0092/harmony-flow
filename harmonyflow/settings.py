1de23fa984968db594bc5b9c0d849273eb4d37d7
dias0092 <muhdias25@gmail.com>
2023-08-07 10:51:06 +0600

    Added account storage settings and Updated yml file

--- harmonyflow/settings.py
+++ harmonyflow/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os 
 from pathlib import Path
+from storages.backends.azure_storage import AzureStorage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AZURE_ACCOUNT_NAME ='harmonyflowstorage'
+AZURE_ACCOUNT_KEY ='hqqvq07uvLBTEmL4AFjsNTmUNeYkBC5Qx4AhFmK0fbNSFd44e9K489cv5ANkVzala9SWBm/xJ0bb+AStdPlbRQ=='
+AZURE_CONTAINER ='audios'
+AZURE_SSL =False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
