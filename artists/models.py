afef37ee97ed8d139aafb97a4ce8d104c5e56080
dias0092 <muhdias25@gmail.com>
2023-08-15 16:36:09 +0600

    Fix bugs in artist app change URLField to ImageField

--- artists/models.py
+++ artists/models.py
@@ -4,7 +4,7 @@ from harmonyflow.storage_backends import AzureMediaStorage
 class Artist(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, null=True, blank=True)
-    image = models.URLField(upload_to='artist_images/', storage=AzureMediaStorage(), null=True, blank=True)
+    image = models.ImageField(upload_to='artist_images/', storage=AzureMediaStorage(), null=True, blank=True)
 
     def __str__(self):
         return self.name
